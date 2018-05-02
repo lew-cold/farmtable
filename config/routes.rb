7f47a6107d5aa0bfcd3c7cab7f67d597581e01c5
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-05-02 18:42:53 +1000

    successful migrate and scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :profiles
   devise_for :users
   resources :products
   root 'products#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_074014) do
+ActiveRecord::Schema.define(version: 2018_05_02_083425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,19 @@ ActiveRecord::Schema.define(version: 2018_05_02_074014) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "users_id"
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street_address"
+    t.string "suburb"
+    t.integer "postcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_profiles_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +55,5 @@ ActiveRecord::Schema.define(version: 2018_05_02_074014) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users", column: "users_id"
 end
