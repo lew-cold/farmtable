0220ba0ea08edb85d24d5e2f2b34bb6d517700de
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-05-08 09:17:02 +1000

    adjusted checkout prompt

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,27 +1,27 @@
 class ChargesController < ApplicationController
     
-
+   
     def create
         Stripe.api_key = ENV["SECRET_KEY"]
         # @amount = (@product.price + @product.shipping).to_i * 100
         # application_fee = (@amount * 0.05).to_i     # 5%
     
         # charge
-        @amount = 100
+        @product = Product.find_by(params[:product_id])
+        @amount = (@product.price + @product.shipping) * 100
         token = params[:stripeToken]
         charge = Stripe::Charge.create({
-          amount: @amount,
-        #   description: @product.product_name,
-        description: 'test',
-          currency: 'aud',
-        #   capture: true,
-          source: token,
+        amount: @amount,
+        description: 'Farm-To-Table pty',
+        currency: 'aud',
+        source: 'tok_visa'
+        destination: { account: "{CONNECTED_STRIPE_ACCOUNT_ID"}
         })
     
         # charge_params[:destination] = User.find(@product.user_id).stripe_id
         # charge_params[:application_fee] = application_fee
     
-        # Stripe::Charge.create(charge_params) # return a Stripe::Charge object
+        # Stripe::Charge.create(charge_params)  # return a Stripe::Charge object
     
     
     
