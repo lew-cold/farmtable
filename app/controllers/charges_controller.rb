74e4a67b90582962032cc6527d352c6b87f562fc
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-05-08 09:31:56 +1000

    resolving issues with stripe(2)

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,33 +1,33 @@
 class ChargesController < ApplicationController
     
    
-    def create
-        Stripe.api_key = ENV["SECRET_KEY"]
-        # @amount = (@product.price + @product.shipping).to_i * 100
-        # application_fee = (@amount * 0.05).to_i     # 5%
+#     def create
+#         Stripe.api_key = ENV["SECRET_KEY"]
+#         # @amount = (@product.price + @product.shipping).to_i * 100
+#         # application_fee = (@amount * 0.05).to_i     # 5%
     
-        # charge
-        @product = Product.find_by(params[:product_id])
-        @amount = (@product.price + @product.shipping) * 100
-        token = params[:stripeToken]
-        charge = Stripe::Charge.create({
-        amount: @amount,
-        description: 'Farm-To-Table pty',
-        currency: 'aud',
-        source: 'tok_visa'
-        destination: { account: "{CONNECTED_STRIPE_ACCOUNT_ID" }
-        })
-        rescue Stripe::CardError => e
-        flash[:error] = e.message
-        redirect_to new_charge_path
-    end
+#         # charge
+#         @product = Product.find_by(params[:product_id])
+#         @amount = (@product.price + @product.shipping) * 100
+#         token = params[:stripeToken]
+#         charge = Stripe::Charge.create({
+#         amount: @amount,
+#         description: 'Farm-To-Table pty',
+#         currency: 'aud',
+#         source: 'tok_visa'
+#         destination: { account: "{CONNECTED_STRIPE_ACCOUNT_ID" }
+#         })
+#         rescue Stripe::CardError => e
+#         flash[:error] = e.message
+#         redirect_to new_charge_path
+#     end
     
-    def thanks
-    end
+#     def thanks
+#     end
     
-      private
-    def set_item
-        @product = Product.find(params[:product_id])
-    end
+#       private
+#     def set_item
+#         @product = Product.find(params[:product_id])
+#     end
    
 end
