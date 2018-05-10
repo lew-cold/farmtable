ad9aa14c220ac8ee3e0266f73d7cfc2a59a39c5e
Lewis Coldwell <lewis@theyorkshirecoder.com>
2018-05-11 08:48:15 +1000

    added validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,9 @@ class Profile < ApplicationRecord
   belongs_to :user, optional: true
   devise :omniauthable, :omniauth_providers => [:stripe_connect]
   validates :user_id, :username, uniqueness: true
+  validates :username, :street_address, :first_name, :last_name, :postcode, :suburb, presence: true
+  validates :postcode, numericality: true
+  
   
 
   def self.search(search)
